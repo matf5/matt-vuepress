8bf6308a9280cbe14fb862b8625b2e1536fbbd59
Tingfeng <tingfeng.ma@shopee.com>
2021-09-15 14:04:59 +0800

    chore: update img

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/matf5/matt-blog.git master:gh-pages
-
+git push -f https://gitee.com/matf5/matt-blog.git master:gh-pages
 cd -
